8e817f324ca9f7206d357fd2806340020ab8e9db
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2023-01-02 09:40:22 +0100

    dependencies

---
+++ python/hw_sim_fwk/updateRequirements.py
@@ -0,0 +1,16 @@
+# alternatively run in your console (with internet connection!):
+# > pipreqs --force ./ --ignore backups
+# ##############################################################
+
+import shlex,  subprocess
+
+
+
+print("Updating requirements.txt..")
+# shell=True generates a high severity vulnerability warning when running bandit - so we set it to False instead..
+p1 = subprocess.Popen(shlex.split("pipreqs --force ./ --ignore backups"), shell=False) # True)
+p1.wait()
+p1.terminate()
+p1.kill()
+
+
